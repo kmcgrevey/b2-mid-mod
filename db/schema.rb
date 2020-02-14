18466e5b256fb79b58c06691b8684f3048074df6
Kevin McGrevey <niv3kmcg@gmail.com>
2020-02-14 10:33:08 -0700

    Create movie object and database

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20200214165047) do
+ActiveRecord::Schema.define(version: 20200214172726) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "movies", force: :cascade do |t|
+    t.string "title"
+    t.integer "creation_year"
+    t.string "genre"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "studios", force: :cascade do |t|
     t.string "name"
