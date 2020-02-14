9af77ce418c69865a5ae83bdebc66288da3797f8
Kevin McGrevey <niv3kmcg@gmail.com>
2020-02-14 11:00:07 -0700

    Add studio modle test and add foreign key to movie table

---
+++ db/migrate/20200214175429_add_studio_to_movies.rb
@@ -0,0 +1,6 @@
+class AddStudioToMovies < ActiveRecord::Migration[5.1]
+  def change
+    add_reference :movies, :studio,
+    foreign_key: true
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20200214172726) do
+ActiveRecord::Schema.define(version: 20200214175429) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -21,10 +21,13 @@ ActiveRecord::Schema.define(version: 20200214172726) do
     t.string "genre"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.bigint "studio_id"
+    t.index ["studio_id"], name: "index_movies_on_studio_id"
   end
 
   create_table "studios", force: :cascade do |t|
     t.string "name"
   end
 
+  add_foreign_key "movies", "studios"
 end
