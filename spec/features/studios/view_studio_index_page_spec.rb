a199d2032435fd1cfce17e4b1f453e5f3623520b
Kevin McGrevey <niv3kmcg@gmail.com>
2020-02-14 09:48:12 -0700

    Add initial test for studio index page

--- spec/features/studios/view_studio_index_page_spec.rb
+++ spec/features/studios/view_studio_index_page_spec.rb
@@ -2,17 +2,20 @@ require 'rails_helper'
 
 RSpec.describe "View Studio Index Page", type: :feature do
   it "I see a list of studios and its movies" do
+    studio1 = Studio.create(name: "Fox")
+    movie1 = Movie.create(name: "Minority Report")
+    movie2 = Movie.create(name: "Ice Age")
+    studio2 = Studio.create(name: "MGM")
+    movie3 = Movie.create(name: "Ben Hur")
+
     visit '/studios'
 
+    expect(current_path).to eq('/studios')
     expect(page).to have_content("Studio Index")
     expect(page).to have_content("Fox")
     expect(page).to have_content("Minority Report")
     expect(page).to have_content("Ice Age")
     expect(page).to have_content("MGM")
     expect(page).to have_content("Ben Hur")
-
-
   end
-
-
 end
